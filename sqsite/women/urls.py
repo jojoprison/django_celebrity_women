d7a503483d194895f71b845e045ed0c83f363d26
George Gavrilov <egyabig2@gmail.com>
2021-07-19 12:36:30 +0300

    video 7 update

--- sqsite/women/urls.py
+++ sqsite/women/urls.py
@@ -5,6 +5,5 @@ from .views import *
 urlpatterns = [
     # имя для маршрута, чтобы юзать во вьюхах
     path('', index, name='home'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
 ]
